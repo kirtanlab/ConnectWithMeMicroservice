55de5d7af3339766ce129ffaf9f10e00b41f1f08
insiyapithapur <banupithapur@gmail.com>
2024-05-17 19:06:57 +0530

    Service Discovery completed

--- APIGateway/src/main/java/com/ConnectWithMe/Main.java
+++ APIGateway/src/main/java/com/ConnectWithMe/Main.java
@@ -1,6 +1,5 @@
 package com.ConnectWithMe;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class Main {
     public static void main(String[] args) {
-         SpringApplication.run(Main.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
--- ServiceDiscovery/src/main/java/com/ConnectWithMe/Main.java
+++ ServiceDiscovery/src/main/java/com/ConnectWithMe/Main.java
@@ -1,14 +1,13 @@
 package com.ConnectWithMe;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class Main {
     public static void main(String[] args) {
-         SpringApplication.run(Main.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
